3eea21c02c356e7d43c02c119be9cd475c63f28e
Shane Lin <xshanelin@gmail.com>
2025-04-04 21:08:02 -0700

    merge sored lists, python

---
+++ grind75/merge-sorted-lists.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        # Create a dummy node to serve as the head of the merged list
+        dummy = ListNode()
+        current = dummy
+        
+        # Traverse both lists and compare values
+        while list1 and list2:
+            if list1.val <= list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current = current.next
+        
+        # Attach remaining nodes from either list
+        if list1:
+            current.next = list1
+        else:
+            current.next = list2
+        
+        return dummy.next
+        
