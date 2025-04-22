1fca6126a58061a77ad3f9e5238a5308528abdb4
Shane Lin <xshanelin@gmail.com>
2025-04-22 09:20:50 -0700

    Merge sorted list, recursive version

---
+++ grind75/merge-sortedlist.py
@@ -0,0 +1,19 @@
+# merge sorted list
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def mergeTwoLists(l1: ListNode, l2: ListNode) -> ListNode:
+    if l1 == None:
+        return l2
+    if l2 == None:
+        return l1
+    if l1.val < l2.val:
+        l1.next = mergeTwoLists(l1.next, l2)
+        return l1
+    else:
+        l2.next = mergeTwoLists(l1, l2.next)
+        return l2
