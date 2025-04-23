a2918f8e74f4bf5e8ca411dca886b6dded650899
Shane Lin <xshanelin@gmail.com>
2025-04-22 19:53:35 -0700

    invert binary tree

---
+++ grind75/invert-binary-tree.py
@@ -0,0 +1,8 @@
+# Given the root of a binary tree, invert the tree, and return its root.
+def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+    if not root:
+        return None
+    root.left, root.right = root.right, root.left
+    self.invertTree(root.left)
+    self.invertTree(root.right)
+    return root
