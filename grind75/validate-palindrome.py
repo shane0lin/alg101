f3be43de16f15f3136d8ab7109cfbe33ee57de70
Shane Lin <xshanelin@gmail.com>
2025-04-22 19:44:34 -0700

    validate palindrome

---
+++ grind75/validate-palindrome.py
@@ -0,0 +1,24 @@
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+def isPalindrome(s):
+    # solution 1
+    # s = s.lower()
+    # s = ''.join(e for e in s if e.isalnum())
+    # return s == s[::-1]
+
+    # solution 2
+    if not s:
+        return True
+    left, right = 0, len(s) - 1
+    while left < right:
+        while left < right and not s[left].isalnum():
+            left += 1
+        while left < right and not s[right].isalnum():
+            right -= 1
+        if s[left].lower() != s[right].lower():
+            return False
+        left += 1
+        right -= 1
+    return True
