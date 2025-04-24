6d367a3ec14592c54a6bdc259ca48c30da78beba
Shane Lin <xshanelin@gmail.com>
2025-04-24 04:18:45 -0700

    Majority Number

---
+++ grind75/majority-element.py
@@ -0,0 +1,11 @@
+# 169. Majority Element
+# Given an array nums of size n, return the majority element.
+# The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+def majorityElement(nums):
+    count = 0
+    candidate = None
+    for num in nums:
+        if count == 0:
+            candidate = num
+        count += (1 if num == candidate else -1)
+    return candidate
