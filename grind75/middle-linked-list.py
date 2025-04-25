0ba87f9ba7a1e04a592937a50530f65c43316cde
Shane Lin <xshanelin@gmail.com>
2025-04-25 06:31:07 -0700

    876 Middle of Linked List

---
+++ grind75/middle-linked-list.py
@@ -0,0 +1,9 @@
+# 876. Middle of the Linked List
+
+def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    slow = head
+    fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+    return slow
