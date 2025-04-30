561266307153e40cf55db5fa390dfa626a2ed0f1
Shane Lin <xshanelin@gmail.com>
2025-04-30 16:06:36 -0700

    fix import in three sum

--- grind75/three-sum.py
+++ grind75/three-sum.py
@@ -14,32 +14,33 @@
 # Constraints:
 # 0 <= nums.length <= 3000
 # -105 <= nums[i] <= 105
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        def two_sum(nums, target, l, r, res):
-            while l < r:
-                if nums[l] + nums[r] == target:
-                    res.append([-target, nums[l], nums[r]])
-                    # ??
-                    l += 1
-                    r -= 1
-                    # while l < r and nums[l] == nums[l - 1]:
-                    #     l += 1
-                    # while l < r and nums[r] == nums[r + 1]:
-                    #     r -= 1
-                elif nums[l] + nums[r] < target:
-                    l += 1
-                else:
-                    r -= 1
-        
-        
-        if len(nums) < 3:
-            return []
-        
-        nums.sort()
-        res = []
-        for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            l, r = i + 1, len(nums) - 1
-            two_sum(nums, -nums[i], l, r, res)
+from typing import List
+
+def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def two_sum(nums, target, l, r, res):
+        while l < r:
+            if nums[l] + nums[r] == target:
+                res.append([-target, nums[l], nums[r]])
+                # ??
+                l += 1
+                r -= 1
+                # while l < r and nums[l] == nums[l - 1]:
+                #     l += 1
+                # while l < r and nums[r] == nums[r + 1]:
+                #     r -= 1
+            elif nums[l] + nums[r] < target:
+                l += 1
+            else:
+                r -= 1
+    
+    
+    if len(nums) < 3:
+        return []
+    
+    nums.sort()
+    res = []
+    for i in range(len(nums)):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+        l, r = i + 1, len(nums) - 1
+        two_sum(nums, -nums[i], l, r, res)
