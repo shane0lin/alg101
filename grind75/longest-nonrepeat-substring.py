2c25f3326266f8e93b3951b51335d1091f72f0e2
Shane Lin <xshanelin@gmail.com>
2025-05-08 08:35:13 -0700

    Add tests for longs nonrepeat substring

--- grind75/longest-nonrepeat-substring.py
+++ grind75/longest-nonrepeat-substring.py
@@ -18,6 +18,8 @@
 # Explanation: The answer is "wke", with the length of 3.
 # Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
+import unittest
+
 def longest_substring(s):
     if not s:
             return 0
@@ -38,3 +40,44 @@ def longest_substring(s):
             l += 1
         r += 1
     return max_len
+
+class TestLongestSubstring(unittest.TestCase):
+    
+    def test_empty_string(self):
+        self.assertEqual(longest_substring(""), 0)
+    
+    def test_single_character(self):
+        self.assertEqual(longest_substring("a"), 1)
+    
+    def test_all_same_characters(self):
+        self.assertEqual(longest_substring("bbbbb"), 1)
+    
+    def test_no_repeating_characters(self):
+        self.assertEqual(longest_substring("abcdef"), 6)
+    
+    def test_example_one(self):
+        self.assertEqual(longest_substring("abcabcbb"), 3)
+    
+    def test_example_two(self):
+        self.assertEqual(longest_substring("bbbbb"), 1)
+    
+    def test_example_three(self):
+        self.assertEqual(longest_substring("pwwkew"), 3)
+    
+    def test_repeating_at_end(self):
+        self.assertEqual(longest_substring("abcdefd"), 6)
+    
+    def test_repeating_at_beginning(self):
+        self.assertEqual(longest_substring("aabcdef"), 6)
+    
+    def test_complex_case(self):
+        self.assertEqual(longest_substring("dvdf"), 3)
+    
+    def test_another_complex_case(self):
+        self.assertEqual(longest_substring("anviaj"), 5)
+    
+    def test_with_spaces(self):
+        self.assertEqual(longest_substring("hello world"), 6)  # "world " is 6 characters
+
+if __name__ == "__main__":
+    unittest.main()
