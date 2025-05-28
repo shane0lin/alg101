cf07009a8ae3497ef3bfd4ee24867d11e71b9cab
Shane Lin <xshanelin@gmail.com>
2025-05-28 05:51:06 -0700

    Fix import Optional

--- grind75/extra/swap-nodes-in-pair.py
+++ grind75/extra/swap-nodes-in-pair.py
@@ -21,12 +21,17 @@
 # Constraints:
 # The number of nodes in the list is in the range [0, 100].
 # 0 <= Node.val <= 100
+
+
 # Definition for singly-linked list.
+from typing import Optional
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
+
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
