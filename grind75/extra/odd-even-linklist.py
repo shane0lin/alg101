3eacf49479dbdc2825219a49b33220c05d6162a3
Shane Lin <xshanelin@gmail.com>
2025-05-28 05:49:53 -0700

    328 Odd Even List

---
+++ grind75/extra/odd-even-linklist.py
@@ -0,0 +1,25 @@
+# 328 Odd Even Linked List
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+        
+class Solution(object):
+    def oddEvenList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        odd = head
+        even = head.next
+        evenHead = even
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+        odd.next = evenHead
+        return head
