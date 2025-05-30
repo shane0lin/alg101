c807019dbe37e664f5267d56887b2d86d52748bb
Shane Lin <xshanelin@gmail.com>
2025-05-30 06:31:43 -0700

    14 Longest Common Prefix

---
+++ grind75/extra/longest-common-prefix.py
@@ -0,0 +1,10 @@
+# 14 Longest Common Prefix
+def longestCommonPrefix(strs):
+    if not strs:
+        return ""
+    shortest = min(strs,key=len)
+    for i, ch in enumerate(shortest):
+        for other in strs:
+            if other[i] != ch:
+                return shortest[:i]
+    return shortest
