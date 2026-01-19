0896ec4e9d5dc0e0602302ebcd6e8efb3ce773e9
Shane <xshanelin@gmail.com>
2026-01-18 21:33:20 -0800

    Rework 2026

---
+++ grind75/l0125/ValidPalindrome.java
@@ -0,0 +1,42 @@
+package grind75.l0020;
+
+public class ValidParentheses {
+    public static boolean isValid(String s) {
+        boolean rst = true;
+        int n = s.length();
+        int left = 0;
+        int right = n-1;
+  
+        while (right > left) {
+            if (!Character.isLetterOrDigit(s.charAt(right))) {
+                right -= 1;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(s.charAt(left))) {
+                left += 1;
+                continue;
+            }
+            if (Character.toLowerCase(s.charAt(right)) == Character.toLowerCase(s.charAt(left))) {
+                right -= 1;
+                left += 1;
+            }
+            else {
+                rst = false;
+                break;
+            }
+        }
+
+        return rst;
+    }
+
+    public static void main () {
+        System.out.println("Hello Java.");
+        System.out.println(isValid("A man, a plan, a canal: Panama"));
+        System.out.println(isValid("aa"));
+        System.out.println(isValid("aba"));
+    }
+
+
+}
+
+
