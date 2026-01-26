da033426a5500a60c2dc127c067c00347138cca9
Shane <xshanelin@gmail.com>
2026-01-26 13:15:04 -0800

    Meeting Rooms

---
+++ grind75/l0252/meeting-rooms.py
@@ -0,0 +1,7 @@
+def canAttendMeetings(intervals: list[list[int]]) -> bool:
+    intervals.sort()
+
+    for i in range(len(intervals) - 1):
+        if intervals[i+1][0] > intervals[i][1]:
+            return False
+    return True
