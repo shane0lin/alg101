55240e74ff7576e3d2637f09cc280cce7dda2752
Shane <xshanelin@gmail.com>
2026-01-27 11:38:46 -0800

    Lowest Common Ancestor of Binary Tree

---
+++ grind75/l0236/lca-binary-tree.py
@@ -0,0 +1,19 @@
+from common.tree import TreeNode
+
+
+def lowestCommonAncestor(root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    if not root or root == p or root == q:
+        return root
+    
+    left = lowestCommonAncestor(root.left, p, q)
+    right = lowestCommonAncestor(root.right, p, q)
+
+    if left and right:
+        return root
+    if left:
+        return left
+    
+    if right:
+        return right
+    
+    
