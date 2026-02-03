48523afe2964fb099ab12c11119eec79c724dd16
Shane <xshanelin@gmail.com>
2026-02-03 11:24:19 -0800

    Coin Change

--- grind75/l0322/coin-change.py
+++ grind75/l0322/coin-change.py
@@ -1,2 +1,11 @@
-def coinChange(self, coins: list[int], amount: int) -> int:
-    pass
+from sys import maxsize
+def coinChange(coins: list[int], amount: int) -> int:
+    dp = [maxsize] * (amount + 1)
+    dp[0] = 0
+    for coin in coins:
+        for i in range(coin, amount+1):
+            dp[i] = min(dp[i], dp[i-coin] + 1) 
+    
+    return dp[amount] if dp[amount] < maxsize else -1
+
+print(coinChange([1,2,5], 11))
