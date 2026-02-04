c01b5345c4785b9a523314f37f4ebaad36eed180
Shane <xshanelin@gmail.com>
2026-02-04 09:20:10 -0800

    Add preorderTransverse for a BST

--- common/tree.py
+++ common/tree.py
@@ -1,5 +1,37 @@
+from typing import List, Optional
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
+    @staticmethod
+    def preorderTraversal(root: Optional['TreeNode']) -> List[int]:
+        result = []
+
+        def dfs(node):
+            if not node:
+                return
+            result.append(node.val)
+            dfs(node.left)
+            dfs(node.right)
+
+        dfs(root)
+        return result
+
+    @staticmethod
+    def preorderTraversalIterative(root: Optional['TreeNode']) -> List[int]:
+        if not root:
+            return []
+        result = []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            result.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return result
