30a08b8df3a38b853b4bbfd6c7712c363994a0e6
Shane <xshanelin@gmail.com>
2026-02-04 09:07:45 -0800

    panlindrome-linked-list

--- grind75/l0234/palindrome-linked-list.py
+++ grind75/l0234/palindrome-linked-list.py
@@ -8,3 +8,18 @@ def isPalindrome(head: Optional[ListNode]) -> bool:
         vals.append(head.val)
         head = head.next
     return vals == vals[::-1]
+
+
+a = ListNode(1)
+b = ListNode(2)
+c = ListNode(5)
+d = ListNode(2)
+e = ListNode(1)
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+
+ListNode.print_list(a)
+print(isPalindrome(a))
+print(isPalindrome(b))
