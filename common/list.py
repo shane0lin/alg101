5d068753a8a4c4e04571f10c8d71f3c10ae280d5
Shane <xshanelin@gmail.com>
2026-02-04 08:47:57 -0800

    Add a utility method to print linkedlist

--- common/list.py
+++ common/list.py
@@ -2,3 +2,12 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+    
+    @staticmethod
+    def print_list(head):
+        rst = []
+        while head:
+            rst.append(head.val)
+            head = head.next
+        print(rst)
+        return rst
