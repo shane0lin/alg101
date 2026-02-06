2c84831961b73bf1684ef325f44eaa705a50dcbe
Shane <xshanelin@gmail.com>
2026-02-06 14:17:40 -0800

    Valid Suduku

---
+++ grind75/l0036/validate-suduko.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+def isValidSudoku(board: List[List[str]]) -> bool:
+    rows = [set() for _ in range(9)]
+    cols = [set() for _ in range(9)]
+    boxes = [set() for _ in range(9)]
+
+    for i in range(9):
+        for j in range(9):
+            val = board[i][j]
+            if val == ".":
+                continue
+
+            box_idx = (i // 3) * 3 + j // 3
+
+            if val in rows[i] or val in cols[j] or val in boxes[box_idx]:
+                return False
+
+            rows[i].add(val)
+            cols[j].add(val)
+            boxes[box_idx].add(val)
+
+    return True
