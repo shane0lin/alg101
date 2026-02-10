4b6f9506f864c5b6dc903c563a01720ab84cbc57
Shane <xshanelin@gmail.com>
2026-02-10 14:15:26 -0800

    Fix some edge cases of rotation array

--- grind75/l0189/rotate-array.py
+++ grind75/l0189/rotate-array.py
@@ -11,14 +11,18 @@ def rotate(nums: List[int], k: int) -> None:
                 left += 1
                 right -= 1
         
-        if not nums:
+        if not nums or k <=0:
             return
         
         n = len(nums)
-
-        reverse(nums, 0, n-k-1)
-        reverse(nums, n-k, n-1)
+        if k % n == 0:
+            return 
+            
+        index = (n-k-1) % n
+        reverse(nums, 0, index)
+        reverse(nums, (index+1) % n, n-1)
         reverse(nums, 0, n-1)
         print(nums)
 
-rotate(nums = [1,2,3,4,5,6,7], k = 3)
+# rotate(nums = [1,2,3,4,5,6,7], k = 3)
+rotate(nums=[1,2], k=2)
