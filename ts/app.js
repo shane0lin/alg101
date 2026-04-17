913cfd21abc87f054b13e6bf863d0c1dc93564db
Shane Lin <xshanelin@gmail.com>
2026-04-16 20:50:47 -0700

    typescript 101

---
+++ ts/app.js
@@ -0,0 +1,6 @@
+"use strict";
+let message = "Hello, Xuanlin!";
+// console.log(message);
+let heading = document.createElement("h1");
+heading.textContent = message;
+document.body.appendChild(heading);
---
+++ ts/app.ts
@@ -0,0 +1,5 @@
+let message: string = "Hello, Xuanlin!";
+// console.log(message);
+let heading = document.createElement("h1");
+heading.textContent = message;
+document.body.appendChild(heading);
