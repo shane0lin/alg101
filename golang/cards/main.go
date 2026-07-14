bcec6f3882a973d1f1428bc6cf2dc59b55072bfa
Shane Lin <xshanelin@gmail.com>
2026-07-13 21:30:27 -0700

    Explore Golang types

---
+++ golang/cards/main.go
@@ -0,0 +1,12 @@
+package main
+import (
+	"fmt"
+)
+func main() {
+	// var n int
+	// fmt.Scan(&n)
+	//var card string = "Ace of Spades"
+	card := "Ace of Spades"
+	card = "Five of Diamond"
+	fmt.Println(card)
+}
