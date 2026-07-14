ebfbf77d8b087a35f23e892c5dfdd1d9d5c5d69d
Shane Lin <xshanelin@gmail.com>
2026-07-14 08:45:08 -0700

    Golang Slice

--- golang/cards/main.go
+++ golang/cards/main.go
@@ -1,13 +1,22 @@
 package main
+
 import (
 	"fmt"
 )
+
 func main() {
 	// var n int
 	// fmt.Scan(&n)
 	//var card string = "Ace of Spades"
 	card := newCard()
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "King of Heart")
 	fmt.Println(card)
+	fmt.Println(cards)
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string {
