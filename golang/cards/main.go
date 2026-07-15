62220fdf5f158b19a7563daf9b0b2a703fad9829
Shane Lin <xshanelin@gmail.com>
2026-07-15 10:33:26 -0700

    Put logic into custom type - deck

--- golang/cards/main.go
+++ golang/cards/main.go
@@ -9,14 +9,15 @@ func main() {
 	// fmt.Scan(&n)
 	//var card string = "Ace of Spades"
 	card := newCard()
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "King of Heart")
 	fmt.Println(card)
 	fmt.Println(cards)
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	// for i, card := range cards {
+	// 	fmt.Println(i, card)
+	// }
+	cards.print()
 }
 
 func newCard() string {
