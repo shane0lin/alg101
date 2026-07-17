eb7a18915f25a37d2c6abe466c43a002e4c4ec52
Shane Lin <xshanelin@gmail.com>
2026-07-17 14:21:09 -0700

    Add unit test for Deck file io

--- golang/cards/deck_test.go
+++ golang/cards/deck_test.go
@@ -1,6 +1,7 @@
 package main
 
 import "testing"
+import "os"
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -14,5 +15,28 @@ func TestNewDeck(t *testing.T) {
 	}
 	if d[len(d)-1] != "Four of Clubs" {
 		t.Errorf("Expected last card to be Four of Clubs, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	// Create a temporary file for testing
+	tmpFile := "_decktesting"
+	// Clean up the temporary file after the test
+	defer os.Remove(tmpFile)
+	// Create a new deck and save it to the temporary file
+
+	d := newDeck()
+	d.saveToFile(tmpFile)
+	// Read the deck back from the temporary file
+	loadedDeck := newDeckFromFile(tmpFile)
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(loadedDeck))
+	}
+	if loadedDeck[0] != "Ace of Spades" {
+		t.Errorf("Expected first card to be Ace of Spades, but got %v", loadedDeck[0])
+	}
+	if loadedDeck[len(loadedDeck)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card to be Four of Clubs, but got %v", loadedDeck[len(loadedDeck)-1])
 	}
 }
